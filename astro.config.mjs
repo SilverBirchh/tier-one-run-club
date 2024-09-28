8e61bfc1ff3c63565ddf8d4bdd051466b04cdd63
Bradley Birch <bradleybirch09@yahoo.co.uk>
2024-09-28 21:10:34 +0100

    logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://tieronerun.club",
   integrations: [
     mdx(),
     sitemap(),
