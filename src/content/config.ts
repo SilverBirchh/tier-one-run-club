0a507d8cf95bb224a97c2c0866feaa376a541769
Bradley Birch <bradleybirch09@yahoo.co.uk>
2024-11-11 18:08:01 +0000

    add comments block

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,26 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: ({ image }) => z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date().optional(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: image(),
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.coerce.date().optional(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image(),
+      includeComments: z.boolean().optional(),
+      comments: z
+        .object({
+          name: z.string(),
+          date: z.string(),
+          comment: z.string(),
+        })
+        .array()
+        .optional(),
+    }),
 });
 
 export const collections = { blog };
